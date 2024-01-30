7c0431423c6e14c957b08709e397a28749e35f2a
Marsboy02 <marsboy0619@gmail.com>
2024-01-30 13:38:18 +0900

    ci: edit init script for raspberry pi os

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # deployment
+
 quipu의 배포를 관리하는 레포지토리입니다.
+
+## nodes
+
+### mars
+
+- 위치 : 서울시립대로 163 학생회관 342호
+- 운영체제 : Raspberry Pi OS 64bit
+
+### venus
+
+- 위치 : 서울시립대로 163 학생회관 342호
+- 운영체제 : Raspberry Pi OS 64bit
--- init.sh
+++ init.sh
@@ -3,10 +3,6 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-echo "apt를 설정합니다."
-apt update
-apt upgrade
-
 echo "apt-get을 설정합니다."
 apt-get update
 apt-get upgrade
@@ -15,8 +11,12 @@ echo "git을 설치합니다."
 apt install git
 
 echo "docker를 설치합니다."
-apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+echo "부팅 시 docker를 자동으로 활용하도록 설정"
+systemctl enable docker
+
+# 라즈베리파이 OS에서는 pip3를 기본적으로 제공한다.
+echo "docker-compose 설치"
+pip3 install docker-compose
